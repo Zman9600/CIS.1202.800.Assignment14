c9d4f3ba8286cf0c8ff7cf4bf9948cd9f78fb4b8
Zman9600 <40652111+Zman9600@users.noreply.github.com>
2021-12-01 13:22:08 -0500

    Wrote test for Vehicle class

--- main.cpp
+++ main.cpp
@@ -1,9 +1,24 @@
+//Zachary Arnould
+//CIS 1202 800
+//December 1 2021
+
 #include <iostream>
+#include "Truck.h"
+#include "Car.h"
+#include "Vehicle.h"
+#include <string>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    string manufacturer;
+    int year;
+    cout << "Enter vehicle manufacturer: ";
+    getline(cin, manufacturer);
+    cout << "Enter vehicle year: ";
+    cin >> year;
+    Vehicle vehicle(manufacturer, year);
+    vehicle.displayInfo();
     return 0;
 }
